809162e4ce4e4aa56ac12e6595e5e2c56897bb9d
rosdev <hagardson6@gmail.com>
2026-04-07 15:27:28 +0200

    hoppas talker2 funkar

--- src/demos/demos/topics/talker2.py
+++ src/demos/demos/topics/talker2.py
@@ -5,19 +5,19 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 class Talker2(Node):
-    def __init__(self2):
+    def __init__(self):
         super().__init__("2")
-        self2.i2 = 0
-        self2.pub2 = self2.create_publisher2(String, "chatter", 10)
+        self.i2 = 0
+        self.pub2 = self.create_publisher2(String, "chatter", 10)
         timer_period2 = 1.0
-        self2.tmr2 = self2.create_timer2(timer_period2, self2.timer_callback2)
+        self.tmr2 = self.create_timer2(timer_period2, self.timer_callback2)
 
-    def timer_callback2(self2):
+    def timer_callback2(self):
         msg2 = String()
-        msg2.data = "Hello robotics students 2: {0}".format(self2.i2)
-        self2.i2 += 1
-        self2.get_logger2().info2('Publishing 2: "{0}"'.format(msg2.data))
-        self2.pub2.publish(msg2)
+        msg2.data = "Hello robotics students 2: {0}".format(self.i2)
+        self.i2 += 1
+        self.get_logger2().info2('Publishing 2: "{0}"'.format(msg2.data))
+        self.pub2.publish(msg2)
 
 def main(args=None):
     rclpy.init(args=args)
