7e09f90e1eb767e42c24d5091f0dfa3f276850bc
rosdev <hagardson6@gmail.com>
2026-04-07 15:31:40 +0200

    hoppas talker2 funka igner

--- src/demos/demos/topics/talker2.py
+++ src/demos/demos/topics/talker2.py
@@ -4,25 +4,25 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 from std_msgs.msg import String
 
-class Talker2(Node):
+class Talker(Node):
     def __init__(self):
-        super().__init__("2")
-        self.i2 = 0
-        self.pub2 = self.create_publisher2(String, "chatter", 10)
-        timer_period2 = 1.0
-        self.tmr2 = self.create_timer2(timer_period2, self.timer_callback2)
+        super().__init__("self")
+        self.i = 0
+        self.pub = self.create_publisher(String, "chatter", 10)
+        timer_period = 1.0
+        self.tmr = self.create_timer(timer_period, self.timer_callback)
 
-    def timer_callback2(self):
-        msg2 = String()
-        msg2.data = "Hello robotics students 2: {0}".format(self.i2)
-        self.i2 += 1
-        self.get_logger2().info2('Publishing 2: "{0}"'.format(msg2.data))
-        self.pub2.publish(msg2)
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Hello robotics students from 2: {0}".format(self.i)
+        self.i += 1
+        self.get_logger().info('Publishing : "{0}"'.format(msg.data))
+        self.pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = Talker2()
+    node = Talker()
 
     try:
         rclpy.spin(node)
